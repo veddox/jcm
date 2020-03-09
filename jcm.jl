a131a4af8c248d932fd0e3313fb0dca5183a994c
Daniel Vedder <d.vedder@web.de>
2020-03-09 12:51:43 +0100

    Added Julia files

---
+++ jcm.jl
@@ -0,0 +1,17 @@
+####
+#### Janzen-Connell Model
+####
+#### A simple forest ecosystem model to investigate the effect of pathogen-
+#### induced density-dependent mortality on tree diversity.
+####
+#### (c) 2020 Daniel Vedder <daniel.vedder@stud-mail.uni-wuerzburg.de>
+####     Licensed under the terms of the MIT license.
+####
+
+module jcm
+
+include("trees.jl")
+include("forest.jl")
+include("pathogens.jl")
+
+end
