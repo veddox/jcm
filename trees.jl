f5c0d7b449de7578f9cf800c1bea4a7db83793da
Daniel Vedder <d.vedder@web.de>
2020-03-10 15:57:48 +0100

    Wrote grow and disperse functions in trees.jl

--- trees.jl
+++ trees.jl
@@ -9,6 +9,7 @@ struct Species
     max_size::UInt8
     growth_rate::UInt8
     seed_production::UInt16
+    dispersal_distance::UInt16
     pathogen_resistance::Float16
 end
 
@@ -21,8 +22,41 @@ mutable struct Tree
 end
 
 #The default species
-Species(id) = Species(id, 500, 25, 1, 50, 0)
+Species(id) = Species(id, 500, 25, 2, 50, 200, 0)
 
 #Create a seed
-Tree(sp, xpos, ypos) = Tree(sp, 0, 0, false, (x=xpos, y=ypos))
-    
+Tree(sp, xpos, ypos) = Tree(sp, 0, 1, false, (x=xpos, y=ypos))
+
+"""
+Produce seeds and disperse them in the landscape, planting them where possible
+"""
+function disperse!(tree::Tree)
+    # Each tree produces multiple seeds
+    for s in 1:tree.species.seed_production
+        #TODO implement a proper dispersal kernel
+        d = tree.species.dispersal_distance
+        sx = tree.position.x + rand(-d:d)
+        sy = tree.position.y + rand(-d:d)
+        if sx >= -worldsize && sx <= worldsize &&
+            sy >= -worldsize && sy <= worldsize
+            planttree!(tree)
+        end
+    end
+end
+
+"""
+All saplings grow until they reach maturity
+"""
+function grow!(cons::Cons=forest)
+    if cons == nothing
+        return
+    else
+        tree = cons.car
+        if !tree.mature && tree.size < tree.species.max_size
+            tree.size += tree.species.growth_rate
+        elseif !tree.mature && tree.size >= tree.species.max_size
+            tree.mature = true
+        end
+        grow!(cons.cdr)
+    end
+end
